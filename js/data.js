be504cedd80d1b8b113dda34ee7b0933fa911bc1
PHM-dung2 <pp4992@naver.com>
2024-12-16 18:19:20 +0900

    js 리스트 추가

--- js/data.js
+++ js/data.js
@@ -1,16 +1,40 @@
+// 매출관리배열 불러오기 함수
 function saleList(){
+
     let saleList = localStorage.getItem('saleList');
-    if( saleList = null ){
+    if( saleList == null ){
         saleList = [
-            { sno : 1 , type : 0 , date : 2024-12-16 , count : 3 , pno : 1 , no : 1 },
-            { sno : 2 , type : 1 , date : 2024-12-15 , count : 2 , pno : 1 , no : 1 },
-            { sno : 3 , type : 0 , date : 2024-12-14 , count : 5 , pno : 2 , no : 2 },
-            { sno : 4 , type : 1 , date : 2024-12-15 , count : 3 , pno : 2 , no : 2 },
-            { sno : 5 , type : 0 , date : 2024-12-14 , count : 10 , pno : 3 , no : 3 }
+            { sno : 1 , type : 0 , date : "2024-12-16" , count : 3 , pno : 1 , no : 1 },
+            { sno : 2 , type : 1 , date : "2024-12-15" , count : 2 , pno : 1 , no : 1 },
+            { sno : 3 , type : 0 , date : "2024-12-14" , count : 5 , pno : 2 , no : 2 },
+            { sno : 4 , type : 1 , date : "2024-12-15" , count : 3 , pno : 2 , no : 2 },
+            { sno : 5 , type : 0 , date : "2024-12-14" , count : 10 , pno : 3 , no : 3 }
         ];
     }else{
         saleList = JSON.parse( saleList );
     }
     return saleList;
+    
+} // f end
+
+// 매출관리배열 저장 함수
+function setSaleList( saleList ){
+    localStorage.setItem('saleList', JSON.stringify( saleList ) );
+} // f end
+
+// 제품번호 샘플
+function productList(){
+
+    let productList = localStorage.getItem('productList');
+    if( productList == null ){
+        productList = [
+            { pno : 1 , pName : "호밀빵" , price : 1000 },
+            { pno : 1 , pName : "든든우유" , price : 2000 },
+            { pno : 1 , pName : "치즈케이크" , price : 3000 }
+        ];
+    }else{
+        productList = JSON.parse( productList );
+    }
+    return productList;
     
 } // f end
--- js/매출관리.js
+++ js/매출관리.js
@@ -1,8 +1,31 @@
 
 outputFunc();
 function outputFunc(){
-    let saleList = saleList();
-    console.log(saleList);
+    let saleArray = saleList();
+    console.log(saleArray);
+    listFunc();
 
-}
+} // f end
+
+function listFunc(){
+    let saleArray = saleList();
+    let tableList = document.querySelector(".tableList")
+    let html = ``;
+    for( let i = 0 ; i < saleArray.length ; i++){
+        let info = saleArray[i];
+        html += `<tr>
+                    <td></td>
+                    <td>${ info.type }</td>
+                    <td>${ info.date }</td>
+                    <td>${ info.count }</td>
+                    <td></td>
+                    <td class="tableBtn">
+                        <button type="button">수정</button>
+                        <button type="button">삭제</button>
+                    </td>
+                </tr>`
+    }
+    tableList.innerHTML = html;
+
+} // f end
 
