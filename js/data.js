7059bde9e8021e357d460e476802d5848bbb5d43
PHM-dung2 <pp4992@naver.com>
2024-12-19 19:30:44 +0900

    -

--- js/data.js
+++ js/data.js
@@ -1095,6 +1095,22 @@ function sampleList(){
     return sampleList;
 }
 
+// 관리자 로그인 정보 리스트
+function sampleAdminList(){
+    let sampleAdminList = localStorage.getItem('sampleAdminList')
+    if( sampleAdminList == null){
+        sampleAdminList = [
+            { ano : 1 , name : '송제영', id : 'admin-01@google.com', pw : '1111'},
+            { ano : 2 , name : '박희만', id : 'admin-02@google.com', pw : '2222'},
+            { ano : 3 , name : '김도하', id : 'admin-03@google.com', pw : '3333'},
+            { ano : 4 , name : '유재석', id : 'admin-04@google.com', pw : '4444'},
+            { ano : 5 , name : '강호동', id : 'admin-05@google.com', pw : '5555'},
+        ];
+    }else{sampleAdminList = JSON.parse(sampleAdminList);        
+        }
+        return sampleAdminList;
+}
+
 // 관리자정보 저장 함수
 function setSampleList( sampleList ){
     localStorage.setItem('sampleList', JSON.stringify( sampleList ) );
@@ -1113,6 +1129,7 @@ function logOutFunc(){
     html = ``;
     document.querySelector('#logIn').innerHTML = html;
 } // f end
+
 
 // // 페이징 함수
 // function pagingFunc( value ){
